956b11ddcb49b38f9ee9c780e3ca7a7c48dc64a5
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 18:17:26 +0530

    refactor: change route

--- src/documentation/AlertDoc/AlertDoc.jsx
+++ src/documentation/AlertDoc/AlertDoc.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
-import { Alert } from "../../components/Alert/Alert";
+import { Alert } from "../../components";
 
 export const AlertDoc = () => {
   document.title = "Attire UI | Alert";
