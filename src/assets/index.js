a4c792d9d0259ee8b61b6a79b60ec41ada2e408b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 15:03:56 +0530

    docs: added images

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+const logo =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1683874845/logo.png";
+const logoIcon =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694154352/Untitled_yoep85.png";
+const header =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694088871/undraw_browsing_re_eycn_mgjktb.svg";
+const avatar =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694326384/photo-1438761681033-6461ffad8d80_ib0x18.jpg";
+const avatarGirl =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694326534/photo-1524250502761-1ac6f2e30d43_j0ka56.jpg";
+const named =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694326864/opz2fzkb02dfso9luxqe.png";
+const womenTop =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694357859/New-Fashion-Women-Short-T-Shirt-Cotton-Cropped-Tops-Women-s-Casual-Crop-Girls-Tops-Summer-Loose-Tops-T-Shirt_fyqw4j.webp";
+const image =
+  "https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694367148/images_cctyz2.jpg";
+
+export { logo, header, logoIcon, avatar, avatarGirl, named, womenTop, image };
