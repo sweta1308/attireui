782f2202b8ff0d527bd7a9f1ea0725d85e87cf53
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 15:05:47 +0530

    feat: add alert component

---
+++ src/components/Alert/Alert.jsx
@@ -0,0 +1,33 @@
+import {
+  CheckCircle,
+  Close,
+  HelpCenter,
+  Info,
+  WarningAmber,
+} from "@mui/icons-material";
+import "./Alert.css";
+
+export const Alert = ({ severity = "success", message }) => {
+  let alertIcon = <CheckCircle />;
+  let alertColor = "var(--green)";
+
+  if (severity === "error") {
+    alertIcon = <Info />;
+    alertColor = "var(--red)";
+  } else if (severity === "warning") {
+    alertIcon = <WarningAmber />;
+    alertColor = "var(--yellow)";
+  } else if (severity === "info") {
+    alertIcon = <HelpCenter />;
+    alertColor = "var(--blue)";
+  }
+
+  return (
+    <div className="alert" style={{ backgroundColor: alertColor }}>
+      <Close className="close-icon" />
+      <div className="alert-text">
+        {alertIcon} <span>{message}</span>
+      </div>
+    </div>
+  );
+};
