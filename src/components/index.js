27b02508869bf7f3425eaa0834b04cbcdcbfd4ad
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 21:23:07 +0530

    feat: addcomponents

--- src/components/index.js
+++ src/components/index.js
@@ -8,6 +8,10 @@ import { SecondaryButton } from "./Buttons/SecondaryButton/SecondaryButton";
 import { LinkButton } from "./Buttons/LinkButton/LinkButton";
 import { IconButton } from "./Buttons/IconButton/IconButton";
 import { FloatingButton } from "./Buttons/FloatingButton/FloatingButton";
+import { Heading } from "./Heading/Heading";
+import { ResponsiveImage } from "./Image/Responsive/Responsive";
+import { RoundedImage } from "./Image/Rounded/Rounded";
+import { Typography } from "./Typography/Typography";
 
 export {
   Alert,
@@ -20,4 +24,8 @@ export {
   LinkButton,
   IconButton,
   FloatingButton,
+  Heading,
+  ResponsiveImage,
+  RoundedImage,
+  Typography,
 };
