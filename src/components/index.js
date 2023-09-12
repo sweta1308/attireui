b418df021a21864333162b14c3e5e81d8f98fa2e
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 18:17:12 +0530

    docs: import and export files

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import { Alert } from "./Alert/Alert";
+import { ImageAvatar } from "./Avatar/ImageAvatar/ImageAvatar";
+import { NamedAvatar } from "./Avatar/NamedAvatar/NamedAvatar";
+import { IconBadge } from "./Badge/IconBadge/IconBadge";
+import { StatusBadge } from "./Badge/StatusBadge/StatusBadge";
+import { PrimaryButton } from "./Buttons/PrimaryButton/PrimaryButton";
+import { SecondaryButton } from "./Buttons/SecondaryButton/SecondaryButton";
+import { LinkButton } from "./Buttons/LinkButton/LinkButton";
+import { IconButton } from "./Buttons/IconButton/IconButton";
+import { FloatingButton } from "./Buttons/FloatingButton/FloatingButton";
+
+export {
+  Alert,
+  ImageAvatar,
+  NamedAvatar,
+  IconBadge,
+  StatusBadge,
+  PrimaryButton,
+  SecondaryButton,
+  LinkButton,
+  IconButton,
+  FloatingButton,
+};
