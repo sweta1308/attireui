7c13abe6ea1ac35ccd694084a65c1eac2ee3c87d
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 21:24:53 +0530

    feat: add search functionality

--- src/documentation/Navbar/Navbar.jsx
+++ src/documentation/Navbar/Navbar.jsx
@@ -1,27 +1,74 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Menu } from "@mui/icons-material";
 import { logo } from "../../assets";
 import "./Navbar.css";
 
 export const NavBar = () => {
   const navigate = useNavigate();
+  const [searchInput, setSearchInput] = useState("");
+
+  const components = [
+    "alert",
+    "avatar",
+    "badge",
+    "button",
+    "card",
+    "heading",
+    "image",
+    "typography",
+  ];
+  const handleClick = () => {
+    if (components.includes(searchInput.toLowerCase())) {
+      navigate(`/docs/${searchInput.toLowerCase()}`);
+    } else {
+      alert(`Oops! ${searchInput} component not found!`);
+    }
+    setSearchInput("");
+  };
+
   return (
     <>
       <nav>
-        <img src={logo} alt="logo" onClick={() => navigate("/")} />
-        <div>
-          <input className="input" placeholder="Search..." />
-          <i className="fa-solid fa-magnifying-glass search"></i>
+        <div className="navigation">
+          <Menu className="menu-icon" />
+          <img src={logo} alt="logo" onClick={() => navigate("/")} />
+          <div className="desktop-search">
+            <input
+              className="input"
+              placeholder="Search..."
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+            <i
+              className="fa-solid fa-magnifying-glass search"
+              onClick={handleClick}
+            ></i>
+          </div>
+          <div className="menu">
+            <a href="/">Home</a>
+            <a href="/components">Components</a>
+            <a
+              href="https://github.com/sweta1308/attireui"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fa-brands fa-github fa-xl"></i>
+            </a>
+          </div>
         </div>
-        <div className="menu">
-          <a href="/">Home</a>
-          <a href="/components">Components</a>
-          <a
-            href="https://github.com/sweta1308/attireui"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fa-brands fa-github fa-xl"></i>
-          </a>
+
+        <div className="mobile-search">
+          <input
+            className="input"
+            placeholder="Search..."
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <i
+            className="fa-solid fa-magnifying-glass search"
+            onClick={handleClick}
+          ></i>
         </div>
       </nav>
     </>
