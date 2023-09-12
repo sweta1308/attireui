bb2dc69f2e1298c7e641e1b65e6bb353ad6f0356
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 21:23:53 +0530

    refactor: updated docs

--- src/documentation/BadgeDoc/BadgeDoc.jsx
+++ src/documentation/BadgeDoc/BadgeDoc.jsx
@@ -6,7 +6,7 @@ import { ImageAvatar, IconBadge, StatusBadge } from "../../components";
 export const BadgeDoc = () => {
   document.title = "Attire UI | Badge";
   return (
-    <div className="docs">
+    <div>
       <h1>Badge</h1>
       <p>
         <span className="colored">Badge</span> is a component which shows the
@@ -27,23 +27,25 @@ export const BadgeDoc = () => {
       </p>
       <p>
         <strong>status: </strong>This is a string value, which will take one
-        value out of online, dnd or offline and will shoe the status
+        value out of online, dnd or offline and will show the status
         accordingly.
       </p>
 
       <h2>Examples</h2>
 
-      <StatusBadge status="online">
-        <ImageAvatar imgSrc={avatar} name="girl" />
-      </StatusBadge>
-      <StatusBadge status="offline">
-        <img
-          width="100rem"
-          height="100rem"
-          src="https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694326864/opz2fzkb02dfso9luxqe.png"
-          alt="named"
-        />
-      </StatusBadge>
+      <div className="docs">
+        <StatusBadge status="online">
+          <ImageAvatar imgSrc={avatar} name="girl" />
+        </StatusBadge>
+        <StatusBadge status="offline">
+          <img
+            width="100rem"
+            height="100rem"
+            src="https://res.cloudinary.com/sweta-agarwalla/image/upload/v1694326864/opz2fzkb02dfso9luxqe.png"
+            alt="named"
+          />
+        </StatusBadge>
+      </div>
 
       <code>
         <iframe
@@ -67,7 +69,7 @@ export const BadgeDoc = () => {
 
       <h2>Props Available</h2>
       <p>
-        <strong>icon: </strong>The icon which we want in the web page.
+        <strong>icon: </strong>The icon which will be shown in the web page.
       </p>
       <p>
         <strong>bgColor: </strong>This will have the backgroundColor of the
@@ -78,14 +80,20 @@ export const BadgeDoc = () => {
         number of items.
       </p>
       <p>
-        <strong>textColor: </strong>This will contain the color which we want
-        the text to have.
+        <strong>textColor: </strong>This will contain the color which the text
+        will have.
       </p>
 
       <h2>Examples</h2>
 
-      <IconBadge icon={<ShoppingCart />} count="5" />
-      <IconBadge icon={<MessageOutlined />} count="12" bgColor="var(--green)" />
+      <div className="docs">
+        <IconBadge icon={<ShoppingCart />} count="5" />
+        <IconBadge
+          icon={<MessageOutlined />}
+          count="12"
+          bgColor="var(--green)"
+        />
+      </div>
 
       <iframe
         src="https://carbon.now.sh/embed?bg=rgba%28176%2C218%2C253%2C1%29&t=lucario&wt=sharp&l=jsx&width=851.8000020980835&ds=true&dsyoff=0px&dsblur=34px&wc=true&wa=false&pv=39px&ph=75px&ln=false&fl=1&fm=Hack&fs=13px&lh=122%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%253CIconBadge%2520icon%253D%257B%253CShoppingCart%2520%252F%253E%257D%2520count%253D%25225%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%253CIconBadge%2520icon%253D%257B%253CMessageOutlined%2520%252F%253E%257D%2520count%253D%252212%2522%2520bgColor%253D%2522var%28--green%29%2522%2520%252F%253E%250A%250A"
--- src/documentation/ButtonDoc/ButtonDoc.jsx
+++ src/documentation/ButtonDoc/ButtonDoc.jsx
@@ -26,26 +26,28 @@ export const ButtonDoc = () => {
       </p>
       <h2>Props Available</h2>
       <p>
-        <strong>buttonText: </strong>This represents the text which we want to
-        show on the button.
+        <strong>buttonText: </strong>This represents the text which will be
+        shown on the button.
       </p>
       <p>
-        <strong>styles: </strong>This will contain all the styles which you want
-        to apply to the button and it will take the styles in the form of an
+        <strong>styles: </strong>This will contain all the styles which will be
+        applied to the button and it will take the styles in the form of an
         object.
       </p>
       <h2>Examples</h2>
-      <PrimaryButton buttonText="Click Me!" />
-      <PrimaryButton
-        buttonText="Submit"
-        styles={{
-          backgroundColor: "blue",
-          color: "yellow",
-          width: "100px",
-          borderRadius: "10px",
-          boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
-        }}
-      />
+      <div className="docs">
+        <PrimaryButton buttonText="Click Me!" />
+        <PrimaryButton
+          buttonText="Submit"
+          styles={{
+            backgroundColor: "blue",
+            color: "yellow",
+            width: "100px",
+            borderRadius: "10px",
+            boxShadow: "0 0 10px rgba(0, 0, 0, 0.5)",
+          }}
+        />
+      </div>
       <code>
         <iframe
           src="https://carbon.now.sh/embed?bg=rgba%28176%2C218%2C253%2C1%29&t=lucario&wt=sharp&l=jsx&width=851.8000020980835&ds=true&dsyoff=0px&dsblur=34px&wc=true&wa=false&pv=39px&ph=75px&ln=false&fl=1&fm=Hack&fs=13px&lh=122%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%253CPrimaryButton%2520buttonText%253D%2522Click%2520Me%21%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%253CPrimaryButton%250A%2520%2520%2520%2520%2520%2520%2520buttonText%253D%2522Submit%2522%250A%2520%2520%2520%2520%2520%2520%2520styles%253D%257B%257B%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520backgroundColor%253A%2520%2522blue%2522%252C%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520color%253A%2520%2522yellow%2522%252C%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520width%253A%2520%2522100px%2522%252C%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520borderRadius%253A%2520%252210px%2522%252C%250A%2520%2520%2520%2520%2520%2520%2520%257D%257D%250A%2520%2520%2520%2520%2520%252F%253E%250A%250A"
@@ -67,14 +69,16 @@ export const ButtonDoc = () => {
       <h2>Props Available</h2>
       <p>Same as Primary Button</p>
       <h2>Examples</h2>
-      <SecondaryButton buttonText="Install App" />
-      <SecondaryButton
-        buttonText="Visit Here"
-        styles={{
-          fontSize: "20px",
-          borderRadius: "0px",
-        }}
-      />
+      <div className="docs">
+        <SecondaryButton buttonText="Install App" />
+        <SecondaryButton
+          buttonText="Visit Here"
+          styles={{
+            fontSize: "20px",
+            borderRadius: "0px",
+          }}
+        />
+      </div>
       <code>
         <iframe
           src="https://carbon.now.sh/embed?bg=rgba%28176%2C218%2C253%2C1%29&t=lucario&wt=sharp&l=jsx&width=851.8000020980835&ds=true&dsyoff=0px&dsblur=34px&wc=true&wa=false&pv=39px&ph=75px&ln=false&fl=1&fm=Hack&fs=13px&lh=122%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%253CSecondaryButton%2520buttonText%253D%2522Install%2520App%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%253CSecondaryButton%250A%2520%2520%2520%2520%2520%2520%2520buttonText%253D%2522Visit%2520Here%2522%250A%2520%2520%2520%2520%2520%2520%2520styles%253D%257B%257B%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520fontSize%253A%2520%252220px%2522%252C%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520borderRadius%253A%2520%25220px%2522%252C%250A%2520%2520%2520%2520%2520%2520%2520%257D%257D%250A%2520%2520%2520%2520%2520%252F%253E%250A%250A"
@@ -96,25 +100,27 @@ export const ButtonDoc = () => {
       </p>
       <h2>Props Available</h2>
       <p>
-        <strong>buttonText: </strong>This represents the text which we want to
-        show on the button.
+        <strong>buttonText: </strong>This represents the text which will be
+        shown on the button.
       </p>
       <p>
-        <strong>linkSrc: </strong>This will contain the URL to which you want
-        the user to navigate.
+        <strong>linkSrc: </strong>This will contain the URL to which the user is
+        navigate.
       </p>
       <p>
-        <strong>styles: </strong>This will contain all the styles which you want
-        to apply to the button and it will take the styles in the form of an
+        <strong>styles: </strong>This will contain all the styles which will be
+        applied to the button and it will take the styles in the form of an
         object.
       </p>
       <h2>Examples</h2>
-      <LinkButton buttonText="Move to Google" linkSrc="https://google.com/" />
-      <LinkButton
-        buttonText="View portfolio"
-        linkSrc="https://swetaagarwalla.netlify.app/"
-        styles={{ color: "green", fontSize: "16px" }}
-      />
+      <div className="docs">
+        <LinkButton buttonText="Move to Google" linkSrc="https://google.com/" />
+        <LinkButton
+          buttonText="View portfolio"
+          linkSrc="https://swetaagarwalla.netlify.app/"
+          styles={{ color: "green", fontSize: "16px" }}
+        />
+      </div>
       <code>
         <iframe
           src="https://carbon.now.sh/embed?bg=rgba%28176%2C218%2C253%2C1%29&t=lucario&wt=sharp&l=jsx&width=851.8000020980835&ds=true&dsyoff=0px&dsblur=34px&wc=true&wa=false&pv=39px&ph=75px&ln=false&fl=1&fm=Hack&fs=13px&lh=122%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%253CLinkButton%2520buttonText%253D%2522Move%2520to%2520Google%2522%2520linkSrc%253D%2522https%253A%252F%252Fgoogle.com%252F%2522%2520%252F%253E%250A%2520%2520%2520%2520%2520%253CLinkButton%250A%2520%2520%2520%2520%2520%2520%2520buttonText%253D%2522View%2520portfolio%2522%250A%2520%2520%2520%2520%2520%2520%2520linkSrc%253D%2522https%253A%252F%252Fswetaagarwalla.netlify.app%252F%2522%250A%2520%2520%2520%2520%2520%2520%2520styles%253D%257B%257B%2520color%253A%2520%2522green%2522%252C%2520fontSize%253A%2520%252216px%2522%2520%257D%257D%250A%2520%2520%2520%2520%2520%252F%253E%250A%250A"
@@ -139,13 +145,15 @@ export const ButtonDoc = () => {
         <strong>icon: </strong>Used to add icon to the component.
       </p>
       <p>
-        <strong>styles: </strong>This will contain all the styles which you want
-        to apply to the button and it will take the styles in the form of an
+        <strong>styles: </strong>This will contain all the styles which will be
+        applied to the button and it will take the styles in the form of an
         object.
       </p>
       <h2>Examples</h2>
-      <IconButton icon={<ShoppingCart />} />
-      <IconButton icon={<Favorite />} styles={{ color: "red" }} />
+      <div className="docs">
+        <IconButton icon={<ShoppingCart />} />
+        <IconButton icon={<Favorite />} styles={{ color: "red" }} />
+      </div>
       <code>
         <iframe
           src="https://carbon.now.sh/embed?bg=rgba%28176%2C218%2C253%2C1%29&t=lucario&wt=sharp&l=jsx&width=851.8000020980835&ds=true&dsyoff=0px&dsblur=34px&wc=true&wa=false&pv=39px&ph=75px&ln=false&fl=1&fm=Hack&fs=13px&lh=122%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%253CIconButton%2520icon%253D%257B%253CShoppingCart%2520%252F%253E%257D%2520%252F%253E%250A%2520%2520%2520%2520%2520%253CIconButton%2520icon%253D%257B%253CFavorite%2520%252F%253E%257D%2520styles%253D%257B%257B%2520color%253A%2520%2522red%2522%2520%257D%257D%2520%252F%253E%250A%250A"
@@ -168,7 +176,9 @@ export const ButtonDoc = () => {
       <h2>Props Available</h2>
       <p>Same as Icon Buttons.</p>
       <h2>Examples</h2>
-      <FloatingButton icon={<EditNote fontSize="large" />} />
+      <div className="docs">
+        <FloatingButton icon={<EditNote fontSize="large" />} />
+      </div>
       <code>
         <iframe
           src="https://carbon.now.sh/embed?bg=rgba%28176%2C218%2C253%2C1%29&t=lucario&wt=sharp&l=jsx&width=851.8000020980835&ds=true&dsyoff=0px&dsblur=34px&wc=true&wa=false&pv=39px&ph=75px&ln=false&fl=1&fm=Hack&fs=13px&lh=122%25&si=false&es=2x&wm=false&code=%2520%2520%2520%2520%2520%253CFloatingButton%2520icon%253D%257B%253CEditNote%2520fontSize%253D%2522large%2522%2520%252F%253E%257D%2520%252F%253E%250A"
