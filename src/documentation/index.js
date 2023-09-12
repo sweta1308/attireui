6e5fb2a9fea103d1a428dbd89f4fbbc6f2d7c735
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 21:24:43 +0530

    feat: add components

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -4,5 +4,20 @@ import { AlertDoc } from "./AlertDoc/AlertDoc";
 import { AvatarDoc } from "./AvatarDoc/AvatarDoc";
 import { BadgeDoc } from "./BadgeDoc/BadgeDoc";
 import { ButtonDoc } from "./ButtonDoc/ButtonDoc";
+import { CardDoc } from "./CardDoc/CardDoc";
+import { HeadingDoc } from "./Heading/HeadingDoc";
+import { ImageDoc } from "./ImageDoc/ImageDoc";
+import { TypographyDoc } from "./TypographyDoc/TypographyDoc";
 
-export { NavBar, Sidebar, AlertDoc, AvatarDoc, BadgeDoc, ButtonDoc };
+export {
+  NavBar,
+  Sidebar,
+  AlertDoc,
+  AvatarDoc,
+  BadgeDoc,
+  ButtonDoc,
+  CardDoc,
+  HeadingDoc,
+  ImageDoc,
+  TypographyDoc,
+};
