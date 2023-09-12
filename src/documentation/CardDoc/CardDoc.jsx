8ad5daa59e65d219b3593c318aa6c3744109aedb
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 21:24:16 +0530

    feat: add card doc component

---
+++ src/documentation/CardDoc/CardDoc.jsx
@@ -0,0 +1,11 @@
+export const CardDoc = () => {
+  return (
+    <>
+      <h1>Card</h1>
+      <p>
+        <span>Card</span> is used to display certain information about a product
+        or a thing.
+      </p>
+    </>
+  );
+};
