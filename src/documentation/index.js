6aad34bb67cfb8033e05fd08cf25496178846e10
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 18:17:16 +0530

    docs: import and export files

---
+++ src/documentation/index.js
@@ -0,0 +1,8 @@
+import { NavBar } from "./Navbar/Navbar";
+import { Sidebar } from "./Sidebar/Sidebar";
+import { AlertDoc } from "./AlertDoc/AlertDoc";
+import { AvatarDoc } from "./AvatarDoc/AvatarDoc";
+import { BadgeDoc } from "./BadgeDoc/BadgeDoc";
+import { ButtonDoc } from "./ButtonDoc/ButtonDoc";
+
+export { NavBar, Sidebar, AlertDoc, AvatarDoc, BadgeDoc, ButtonDoc };
