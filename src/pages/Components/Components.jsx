e3221cc7df7b21fe4b273b14674507a0575f416b
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 15:07:09 +0530

    feat: add component page

---
+++ src/pages/Components/Components.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import "./Components.css";
+
+export const Components = () => {
+  document.title = "Attire UI | Components";
+  return (
+    <>
+      <div className="component">
+        <h1>Components</h1>
+        <div className="components">
+          <ul>
+            <li>
+              <Link to="/docs/alert">Alert</Link>
+            </li>
+            <li>
+              <Link to="/docs/avatar">Avatar</Link>
+            </li>
+            <li>
+              <Link>Badge</Link>
+            </li>
+            <li>
+              <Link>Buttons</Link>
+            </li>
+            <li>
+              <Link>Card</Link>
+            </li>
+            <li>
+              <Link>Headings</Link>
+            </li>
+            <li>
+              <Link>Image</Link>
+            </li>
+            <li>
+              <Link>Text</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
