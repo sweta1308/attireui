ca28e471a1f92963f3fe322b58a0651bbebf5101
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-12 15:06:46 +0530

    feat: add sidebar component

---
+++ src/documentation/Sidebar/Sidebar.jsx
@@ -0,0 +1,46 @@
+import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
+
+export const Sidebar = () => {
+  const activeStyles = ({ isActive }) => ({
+    color: isActive ? "#e80071" : "",
+    fontWeight: isActive ? "600" : "",
+  });
+  return (
+    <>
+      <div className="sidebar">
+        <h3>Components</h3>
+        <ul>
+          <li>
+            <NavLink to="/docs/alert" style={activeStyles}>
+              Alert
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/docs/avatar" style={activeStyles}>
+              Avatar
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Badge</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Button</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Card</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Heading</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Image</NavLink>
+          </li>
+          <li>
+            <NavLink to="/">Typography</NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
