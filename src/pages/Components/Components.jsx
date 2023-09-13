1a762480b2800c97a79555049de19a9c56fb4e68
Sweta Agarwalla <swetaagarwalla13@gmail.com>
2023-09-13 17:52:25 +0530

    refactor: components page

--- src/pages/Components/Components.jsx
+++ src/pages/Components/Components.jsx
@@ -1,38 +1,23 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./Components.css";
 
 export const Components = () => {
   document.title = "Attire UI | Components";
+  const navigate = useNavigate();
   return (
     <>
       <div className="component">
         <h1>Components</h1>
         <div className="components">
           <ul>
-            <li>
-              <Link to="/docs/alert">Alert</Link>
-            </li>
-            <li>
-              <Link to="/docs/avatar">Avatar</Link>
-            </li>
-            <li>
-              <Link to="/docs/badge">Badge</Link>
-            </li>
-            <li>
-              <Link to="/docs/button">Buttons</Link>
-            </li>
-            <li>
-              <Link to="/docs/card">Card</Link>
-            </li>
-            <li>
-              <Link to="/docs/heading">Headings</Link>
-            </li>
-            <li>
-              <Link to="/docs/image">Image</Link>
-            </li>
-            <li>
-              <Link to="/docs/typography">Typography</Link>
-            </li>
+            <li onClick={() => navigate("/docs/alert")}>Alert</li>
+            <li onClick={() => navigate("/docs/avatar")}>Avatar</li>
+            <li onClick={() => navigate("/docs/badge")}>Badge</li>
+            <li onClick={() => navigate("/docs/button")}>Buttons</li>
+            <li onClick={() => navigate("/docs/card")}>Card</li>
+            <li onClick={() => navigate("/docs/heading")}>Headings</li>
+            <li onClick={() => navigate("/docs/image")}>Image</li>
+            <li onClick={() => navigate("/docs/typography")}>Typography</li>
           </ul>
         </div>
       </div>
